c3a4059e791bf392eb44a5a12b45a43a8b31dee4
Elyar <elyareranjbar@gmail.com>
2025-09-24 21:46:27 +0330

    Related models

---
+++ src/Models/Abstract/Order.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace TelegramBotEssentials\Billing\Models\Abstract;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
+use TelegramBotEssentials\Essence\Models\BotUser;
+use TelegramBotEssentials\Essence\Traits\HasInvoice;
+use TelegramBotEssentials\Essence\Traits\HasMessageMeta;
+
+abstract class Order extends Model
+{
+    use BelongsToTenant;
+    use HasMessageMeta;
+    use HasInvoice;
+
+    protected $appends = ['amount', 'description', 'paid_at'];
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function botUser(): BelongsTo
+    {
+        return $this->belongsTo(BotUser::class);
+    }
+
+    abstract public function getPaidAtAttribute(): ?Carbon;
+
+    abstract public function getAmountAttribute(): string;
+
+    abstract public function getDescriptionAttribute(): string;
+}
---
+++ src/Models/Attempts/ToZirgozarAttempt.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TelegramBotEssentials\Billing\Models\Attempts;
+
+use TelegramBotEssentials\Billing\Models\Abstract\PaymentAttempt;
+
+class ToZirgozarAttempt extends PaymentAttempt
+{
+    protected function attemptSucceedHook(): void
+    {
+        // TODO: Implement attemptSucceedHook() method.
+    }
+
+    protected function attemptFailedHook(): void
+    {
+        // TODO: Implement attemptFailedHook() method.
+    }
+}
