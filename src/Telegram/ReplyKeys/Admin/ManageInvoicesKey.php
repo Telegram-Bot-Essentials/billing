9c6a8f379bb60482b012d83fb96a2c3ae655681e
Elyar <elyareranjbar@gmail.com>
2025-09-24 21:48:13 +0330

    Telegram logic of the Invoicing

---
+++ src/Telegram/ReplyKeys/Admin/ManageInvoicesKey.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TelegramBotEssentials\Billing\Telegram\ReplyKeys\Admin;
+
+use TelegramBotEssentials\Billing\Telegram\Features\Admin\ManageInvoicesFeature;
+use TelegramBotEssentials\Essence\Enums\Roles;
+use TelegramBotEssentials\Essence\Telegram\ReplyKeys\ReplyKey;
+
+class ManageInvoicesKey extends ReplyKey
+{
+    protected string $text = 'Manage Invoices';
+    protected int $perm = Roles::ADMIN->value;
+    protected string $response = 'Manage Invoices executed successfully.';
+
+    public function __construct()
+    {
+        // Multilingual translations
+        // $this->text = __('');
+        // $this->response = __('');
+    }
+
+    public function handle(): void
+    {
+        ManageInvoicesFeature::menu()->send();
+        // Logic to execute
+    }
+}
---
+++ src/Telegram/StateAnswers/Admin/ManageInvoicesAnswer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TelegramBotEssentials\Billing\Telegram\StateAnswers\Admin;
+
+use TelegramBotEssentials\Essence\Enums\AllowableFields;
+use TelegramBotEssentials\Essence\Enums\Roles;
+use TelegramBotEssentials\Essence\Telegram\StateAnswers\StateAnswer;
+
+class ManageInvoicesAnswer extends StateAnswer
+{
+    protected string $type = 'MANAGEINVOICES';
+    protected int $perm = Roles::ADMIN->value;
+    protected array $allowedFields = [
+        AllowableFields::TEXT->value
+    ];
+
+    public function handle(string $method): void
+    {
+        switch (strtolower($method)) {
+
+        }
+    }
+}
