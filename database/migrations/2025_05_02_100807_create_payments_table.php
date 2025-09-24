12b37a7108bee217424377b772897c036e0bde56
Elyar <elyareranjbar@gmail.com>
2025-09-24 21:46:06 +0330

    Related migrations

---
+++ database/migrations/2025_05_02_100807_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use TelegramBotEssentials\Billing\Models\Invoice;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// TODO: remove this migration
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Invoice::class)->constrained()->cascadeOnDelete();
+
+            $table->nullableMorphs('attempt');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
