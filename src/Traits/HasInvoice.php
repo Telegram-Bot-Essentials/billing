3da55892626d68b3cf24d99a72c1f10c2468edd0
Elyar <elyareranjbar@gmail.com>
2025-09-24 21:47:56 +0330

    HasInvoice trait for Invoice relations

---
+++ src/Traits/HasInvoice.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TelegramBotEssentials\Billing\Traits;
+
+use TelegramBotEssentials\Billing\Models\Invoice;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+trait HasInvoice
+{
+    public function invoice(): MorphOne
+    {
+        return $this->morphOne(Invoice::class, 'payable')->latest();
+    }
+
+    abstract public function invoicePaidHook(): void;
+
+    abstract public function cancelOrderHook(): void;
+
+    public function invoicePendingHook(): void
+    {
+        // Optional hooks
+    }
+
+    public function invoiceFailedHook(): void
+    {
+        // Optional hooks
+    }
+}
