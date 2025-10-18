8005d54e7425882ab15316dfe637a2cb5a4b76be
Elyar <elyareranjbar@gmail.com>
2025-10-18 20:41:39 +0330

    Key organization

--- src/Telegram/Features/Admin/ManageInvoicesFeature.php
+++ src/Telegram/Features/Admin/ManageInvoicesFeature.php
@@ -61,6 +61,15 @@ class ManageInvoicesFeature
         );
     }
 
+    private static function statusIndicator(?string $status): string
+    {
+        return match ($status) {
+            'paid' => __('tbe-billing::manage_invoices.main.keys.status_indicator.paid'),
+            'failed' => __('tbe-billing::manage_invoices.main.keys.status_indicator.failed'),
+            default => __('tbe-billing::manage_invoices.main.keys.status_indicator.pending'),
+        };
+    }
+
     public static function show(Invoice $invoice, int $lastPage = 1): TelegramResponse
     {
         $statusIndicator = self::statusIndicator($invoice->status);
@@ -82,21 +91,26 @@ class ManageInvoicesFeature
 
         $selectedSuffix = __('tbe-billing::manage_invoices.main.keys.status_selected_suffix');
 
+
+        $replyMarkup->row([
+            Keyboard::inlineButton([
+                'text' => __('tbe-billing::manage_invoices.main.keys.status_paid')
+                    . ($invoice->status == 'paid' ? $selectedSuffix : ''),
+                'callback_data' => encodeCallback(self::$type, 'mark_as_paid', [$invoice->id, $lastPage])
+            ])
+        ]);
+        $replyMarkup->row([
+            Keyboard::inlineButton([
+                'text' => __('tbe-billing::manage_invoices.main.keys.status_pending')
+                    . ($invoice->status == 'pending' ? $selectedSuffix : ''),
+                'callback_data' => encodeCallback(self::$type, 'mark_as_pending', [$invoice->id, $lastPage])
+            ])
+        ]);
         $replyMarkup->row([
             Keyboard::inlineButton([
                 'text' => __('tbe-billing::manage_invoices.main.keys.status_failed')
                     . ($invoice->status == 'failed' ? $selectedSuffix : ''),
                 'callback_data' => encodeCallback(self::$type, 'mark_as_failed', [$invoice->id, $lastPage])
-            ]),
-            Keyboard::inlineButton([
-                'text' => __('tbe-billing::manage_invoices.main.keys.status_pending')
-                    . ($invoice->status == 'pending' ? $selectedSuffix : ''),
-                'callback_data' => encodeCallback(self::$type, 'mark_as_pending', [$invoice->id, $lastPage])
-            ]),
-            Keyboard::inlineButton([
-                'text' => __('tbe-billing::manage_invoices.main.keys.status_paid')
-                    . ($invoice->status == 'paid' ? $selectedSuffix : ''),
-                'callback_data' => encodeCallback(self::$type, 'mark_as_paid', [$invoice->id, $lastPage])
             ])
         ]);
 
@@ -112,14 +126,5 @@ class ManageInvoicesFeature
             replyMarkup: $replyMarkup,
             parseMode: 'HTML'
         );
-    }
-
-    private static function statusIndicator(?string $status): string
-    {
-        return match ($status) {
-            'paid' => __('tbe-billing::manage_invoices.main.keys.status_indicator.paid'),
-            'failed' => __('tbe-billing::manage_invoices.main.keys.status_indicator.failed'),
-            default => __('tbe-billing::manage_invoices.main.keys.status_indicator.pending'),
-        };
     }
 }
