a36217ec48da626adbaa63296aced9d0b67eb807
Elyar <elyareranjbar@gmail.com>
2025-10-20 19:37:29 +0330

    Currency father service

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,7 @@
 
 use TelegramBotEssentials\Billing\Services\Billing;
 use TelegramBotEssentials\Billing\Services\Currency;
+use TelegramBotEssentials\Billing\Services\CurrencyFather;
 use TelegramBotEssentials\Billing\Services\Gateways;
 
 if (!function_exists('billing')) {
@@ -22,5 +23,12 @@ if (!function_exists('gateways')) {
     function gateways(): Gateways
     {
         return app(Gateways::class);
+    }
+}
+
+if (!function_exists('priceIn')) {
+    function priceIn(string $price): CurrencyFather
+    {
+        return CurrencyFather::from(settings()->get('billing.currency'))->amount($price);
     }
 }
